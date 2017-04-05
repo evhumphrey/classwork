1e3d13cceebe96be98297b96ca1188863aa5010e
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-05 15:37:16 -0700

    Add tests for TowersOfHanoi, more skeleton code in class

--- tower_of_hanoi/lib/towers_of_hanoi.rb
+++ tower_of_hanoi/lib/towers_of_hanoi.rb
@@ -1,2 +1,13 @@
 class TowersOfHanoi
+  attr_reader :towers
+
+  def initialize
+  end
+
+  def move(start_tower, end_tower)
+  end
+
+  def won?
+  end
+
 end
--- tower_of_hanoi/spec/towers_of_hanoi_spec.rb
+++ tower_of_hanoi/spec/towers_of_hanoi_spec.rb
@@ -2,9 +2,47 @@ require 'rspec'
 require 'towers_of_hanoi'
 
 describe TowersOfHanoi do
+  subject(:game) { TowersOfHanoi.new }
+  let(:start_tower_state) { [[1, 2, 3], [0, 0, 0], [0, 0, 0]] }
+  context '#initialize' do
+    it 'sets towers' do
+      expect(game.towers).to_eq(start_tower_state)
+    end
+  end
+
   context '#move' do
+    let(:same_tower_error) { "Invalid move: can't move on same tower" }
+    let(:big_on_small_error) { "Invalid move: can't place bigger disk on smaller disk" }
+
+    it 'prevents user from moving disk from a tower to the same tower' do
+      expect { game.move(0, 0) }.to raise_error(same_tower_error)
+    end
+
+    it 'prevents user from moving bigger disk on to a smaller disk' do
+      game.move(0, 1)
+      expect { game.move(0, 1) }.to raise_error(big_on_small_error)
+    end
+
+    it 'moves a disk from one tower to another' do
+      game.move(0, 1)
+      expect(game.towers).to_eq([0, 2, 3], [0, 0, 1], [0, 0, 0])
+    end
   end
 
   context '#won?' do
+    let(:win_one) { [[0, 0, 0], [1, 2, 3], [0, 0, 0]] }
+    let(:win_two) { [[0, 0, 0], [0, 0, 0], [1, 2, 3]] }
+
+    it 'returns true when second tower has complete stack of disks' do
+      expect(game.towers == win_one).to eq(game.won?)
+    end
+
+    it 'returns true when second tower has complete stack of disks' do
+      expect(game.towers == win_two).to eq(game.won?)
+    end
+
+    it 'returns false when starting tower has complete stack' do
+      expect(game.won?).to be false
+    end
   end
 end
