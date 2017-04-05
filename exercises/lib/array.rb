fae3dc44d3ef49bb3a5ffe0e826e05e5ca53ebb4
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-05 13:44:39 -0700

    Implement Array#my_transpose

--- exercises/lib/array.rb
+++ exercises/lib/array.rb
@@ -27,6 +27,22 @@ class Array
   end
 
   def my_transpose
+    # when self is a one-dimensional array, raise error
+    unless self.all? { |el| el.is_a?(Array) }
+      raise TypeError
+    end
+
+    transposed = []
+
+    (0...length).each do |col_idx|
+      new_row = []
+      (0...length).each do |row_idx|
+        new_row << self[row_idx][col_idx]
+      end
+      transposed << new_row
+    end
+
+    transposed
   end
 
 end
