2cf143e4bc893f7a612c3c1617f4207e1ec6c9ec
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-05 16:17:41 -0700

    Implement (to pass tests) TowersOfHanoi

--- tower_of_hanoi/lib/towers_of_hanoi.rb
+++ tower_of_hanoi/lib/towers_of_hanoi.rb
@@ -2,12 +2,41 @@ class TowersOfHanoi
   attr_reader :towers
 
   def initialize
+    # treat as psuedo-stacks
+    @towers = [
+      [3, 2, 1],
+      [],
+      []
+    ]
   end
 
   def move(start_tower, end_tower)
+    # errors
+    if start_tower == end_tower
+      raise "Invalid move: can't move on same tower"
+    end
+    if peek(start_tower).nil?
+      raise "Invalid move: tower has no disks to move"
+    end
+    unless peek(end_tower).nil?
+      if peek(start_tower) > peek(end_tower)
+        raise "Invalid move: can't place bigger disk on smaller disk"
+      end
+    end
+
+    @towers[end_tower].push(@towers[start_tower].pop)
   end
 
   def won?
+    true if @towers[1].length == 3 || @towers[2].length == 3
+    false
+  end
+
+  private
+
+  def peek(tower_num)
+    return @towers[tower_num].last unless @towers[tower_num].empty?
+    nil
   end
 
 end
