6dacfaa3d84dd076c065dc316c147a5410baf5c0
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-05 17:05:12 -0700

    Add Card#initialize tests

--- poker/spec/card_spec.rb
+++ poker/spec/card_spec.rb
@@ -2,5 +2,17 @@ require 'rspec'
 require 'card'
 
 describe Card do
-  
+  subject(:card) { Card.new(5, :heart) }
+
+  context '#initialize' do
+
+    it 'sets the card value' do
+      expect(card.value).to eq(5)
+    end
+
+    it 'sets the card suit' do
+      expect(card.suit).to eq(:heart)
+    end
+  end
+
 end
