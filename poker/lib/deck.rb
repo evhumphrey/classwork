b0fd1b655a1c1d725046f0b5639be02e566c328f
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-05 17:59:00 -0700

    Implement Deck#initialize to pass tests

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,3 +1,25 @@
 class Deck
 
+  attr_reader :cards
+
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+  SUITS = [:club, :diamond, :heart, :spade]
+
+  def initialize
+    @cards = populate_deck
+  end
+
+  private
+
+  def populate_deck
+    deck = []
+
+    VALUES.each do |value|
+      SUITS.each do |suit|
+        deck << Card.new(value, suit)
+      end
+    end
+
+    deck
+  end
 end
