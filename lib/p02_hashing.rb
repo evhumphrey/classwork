37b45127006a4f51ba0a5b54561d0499dc62bc5b
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-07 13:32:36 -0700

    Implement hashing function

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -6,7 +6,8 @@ class Array
   # each el (Fixnum); mutliply index.hash by element; add all results
   def hash
     hashing_sum = map.with_index do |el, i|
-      i.hash * el
+      el = el.ord if el.is_a?(String) # handles strings as elements
+      i.hash * el.hash
     end
 
     hashing_sum.reduce(:+).hash
@@ -14,15 +15,22 @@ class Array
 end
 
 class String
+  BIG_PRIME = 2147483647
   def hash
-    
+    # convert stirng to array
+    # get hash of array
+    # multiply hashing result by 2147483647 (prime)
+    self.chars.hash * BIG_PRIME
   end
 end
 
 class Hash
+  MERSENNE_PRIME = 2305843009213693951
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    #determine order of the hash
+    # p self.to_a.sort.hash
+    self.to_a.sort.hash * MERSENNE_PRIME
   end
 end
