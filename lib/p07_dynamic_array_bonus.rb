ee77ea91061e8494d7fb79c947f2c019cc492b1f
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-07 17:40:43 -0700

    Attempt to build dynamic array

--- lib/p07_dynamic_array_bonus.rb
+++ lib/p07_dynamic_array_bonus.rb
@@ -25,6 +25,9 @@ class StaticArray
 end
 
 class DynamicArray
+
+  include Enumerable
+
   attr_reader :count
 
   def initialize(capacity = 8)
@@ -33,9 +36,12 @@ class DynamicArray
   end
 
   def [](i)
+    return nil unless i.between?(0, count-1)
+    @store[i]
   end
 
   def []=(i, val)
+    @store[i] = val
   end
 
   def capacity
@@ -43,27 +49,59 @@ class DynamicArray
   end
 
   def include?(val)
+    self.each do |el|
+      return true if el == val
+    end
+    false
   end
 
   def push(val)
+    if count == @store.length
+      resize!
+    end
+    self[count] = val
+    @count += 1
   end
 
   def unshift(val)
+
+    # p "COUNT: #{count} | STORE_LENGTH: #{@store.length}"
+    if count == @store.length
+      resize!
+    end
+    (count-1).downto(0) do |i|
+      self[i + 1] = self[i]
+      p self
+    end
+
+    @count += 1
+    self[0] = val
+    p self
   end
 
   def pop
+    return nil if count == 0
+    popped = last
+    self[count-1] = nil
+    @count -= 1
+    popped
   end
 
   def shift
   end
 
   def first
+    self[0]
   end
 
   def last
+    self[count-1]
   end
 
-  def each
+  def each(&prc)
+    (0...count).each do |i|
+      prc.call(self[i])
+    end
   end
 
   def to_s
@@ -81,5 +119,12 @@ class DynamicArray
   private
 
   def resize!
+    copy_store = StaticArray.new(capacity * 2)
+
+    self.each_with_index do |el, i|
+      copy_store[i] = self[i]
+    end
+
+    @store = copy_store
   end
 end
