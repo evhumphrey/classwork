113c121cb2238f1e83e0e2a06815eb6b0a8be19d
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-11 15:07:17 -0700

    Implement Question::find_question_by_author_id query

---
+++ question.rb
@@ -0,0 +1,23 @@
+require_relative 'questions_database'
+
+class Question
+  def self.find_by_author_id(author_id)
+    query = QuestionsDbConnection.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = ?
+    SQL
+    query.map { |data| Question.new(data) }
+  end
+
+  attr_accessor :title, :body, :author_id
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+end
