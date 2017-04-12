c8a8e7c65aa576189031489eee6e2f49f6990b3e
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-12 11:54:07 -0700

    Add seed, add validations for User model

--- URLShortener/db/seeds.rb
+++ URLShortener/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(email: 'fake@gmail.com')
+user2 = User.create(email: 'grandma@aol.com')
+user3 = User.create(email: 'hottie@hotmail.com')
+user4 = User.create(email: 'woohoo@yahoo.com')
+user5 = User.create(email: 'myself@me.com')
