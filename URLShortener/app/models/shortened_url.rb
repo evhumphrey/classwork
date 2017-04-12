4a27fdf3042639c95859cba30e1b2c11c1ca3d2e
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-12 12:16:59 -0700

    Add validations for ShortenedUrl

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,2 +1,5 @@
 class ShortenedUrl < ApplicationRecord
+  validates :long_url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true
+  validates :submitter_id, presence: true
 end
--- URLShortener/db/seeds.rb
+++ URLShortener/db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 user1 = User.create(email: 'fake@gmail.com')
 user2 = User.create(email: 'grandma@aol.com')
 user3 = User.create(email: 'hottie@hotmail.com')
 user4 = User.create(email: 'woohoo@yahoo.com')
 user5 = User.create(email: 'myself@me.com')
+
+ShortenedUrl.destroy_all
+shortened_url1 = ShortenedUrl.create(
+  long_url: "www.google.com",
+  short_url: "goog.gl",
+  submitter_id: user1.id
+)
