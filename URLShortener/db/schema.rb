b9d2dd41b1b0654e212558b1e00a047d27e7dac2
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-12 16:31:09 -0700

    Migrate taggings and tag_topics

--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412210951) do
+ActiveRecord::Schema.define(version: 20170412232632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(version: 20170412210951) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["long_url", "short_url"], name: "index_shortened_urls_on_long_url_and_short_url", unique: true, using: :btree
+  end
+
+  create_table "tag_topics", force: :cascade do |t|
+    t.string   "topic",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic"], name: "index_tag_topics_on_topic", unique: true, using: :btree
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",     null: false
+    t.integer  "link_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id", "link_id"], name: "index_taggings_on_tag_id_and_link_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
