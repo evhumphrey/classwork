2feef463b5a08b70fdc3cd9155d1d2c4447b0720
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-12 13:54:00 -0700

    Implement ShortenedUrl::rand_code, ShortenedUrl::shortenify; add ShortenedUrl submitter association

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,5 +1,28 @@
 class ShortenedUrl < ApplicationRecord
+  include SecureRandom
+
   validates :long_url, presence: true, uniqueness: true
   validates :short_url, presence: true, uniqueness: true
   validates :submitter_id, presence: true
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64(5)
+    while self.exists?(short_url: code)
+      code = SecureRandom.urlsafe_base64(5)
+    end
+    code
+  end
+
+  def self.shortenify(user, long_url)
+    self.create!(
+      long_url: long_url,
+      submitter_id: user.id,
+      short_url: "tiny.io/#{self.random_code}"
+    )
+  end
+
+  belongs_to :submitter,
+    primary_key: :id,
+    foreign_key: :submitter_id,
+    class_name: :User
 end
