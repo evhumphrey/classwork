cec3b106a0da8c553e5f8702bc21ba872302bedf
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-12 10:59:31 -0700

    Add Course enrollments and enrolled_students associations

--- associations_exercise/app/models/course.rb
+++ associations_exercise/app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ActiveRecord::Base
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
 end
