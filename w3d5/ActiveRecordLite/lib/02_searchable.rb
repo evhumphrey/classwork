b2c27c23d13eb88ba628f8d84d2ea59beae00020
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-14 16:23:03 -0700

    Implement Phase2: Searchable#where

--- w3d5/ActiveRecordLite/lib/02_searchable.rb
+++ w3d5/ActiveRecordLite/lib/02_searchable.rb
@@ -3,10 +3,25 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    values_list = params.values
+    where_line = params.keys.map do |attr_name|
+      "#{attr_name} = ?"
+    end
+    where_line = where_line.join(' AND ')
+
+    query = DBConnection.execute(<<-SQL, *values_list)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(query)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
