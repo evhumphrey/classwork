089c3d41d3415bb7a51834bbefeaff0dafc861cc
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-14 12:08:25 -0700

    Implement Phase0: AttrAccessorObject::my_attr_accessor

--- w3d5/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ w3d5/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,24 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    # go through each name (symbol) passed in (ie :x, :y)
+    names.each do |name|
+
+      # convert name (:x) to correct format for instance_* method (:@x)
+      instance_name = "@#{name}".to_sym
+
+      # define the getter (:x)
+      getter_name = name
+      define_method(getter_name) do
+        # pass expected argument (:@x)
+        instance_variable_get(instance_name)
+      end
+
+      # define the setter (:x=)
+      setter_name = "#{name}=".to_sym
+      define_method(setter_name) do |set_value|
+        # pass expected argument (@:x, set_value))
+        instance_variable_set(instance_name, set_value)
+      end
+    end
   end
 end
