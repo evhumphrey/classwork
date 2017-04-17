144e599bf8a278e8f474c0f7bfcbac098702b279
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-17 13:43:22 -0700

    add routes for user

--- FirstRoutes/config/routes.rb
+++ FirstRoutes/config/routes.rb
@@ -53,4 +53,15 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # resources :users
+
+  get 'users/', to: 'users#index', as: :users
+  post 'users/', to: 'users#create'
+  get 'users/new', to: 'users#new', as: :new_user
+  get 'users/:id/edit', to: 'users#edit', as: :edit_user
+  get 'users/:id', to: 'users#show', as: :user
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 end
