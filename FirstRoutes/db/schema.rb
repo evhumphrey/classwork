8d9b2cfd1da898a8fd8bfb13146f0125ce00110d
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-17 18:02:20 -0700

    create Artwork model and table and associations

--- FirstRoutes/db/schema.rb
+++ FirstRoutes/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418002334) do
+ActiveRecord::Schema.define(version: 20170418003806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "image_url",  null: false
+    t.integer  "artist_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "artworks", ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
